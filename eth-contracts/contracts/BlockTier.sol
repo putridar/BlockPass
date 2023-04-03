bba700bc273e2810d240194b24c823cb924d5748
kennyhw <kennyhermawan7@gmail.com>
2023-04-03 17:02:09 +0800

    Complete BlockTier

--- eth-contracts/contracts/BlockTier.sol
+++ eth-contracts/contracts/BlockTier.sol
@@ -14,15 +14,40 @@ contract BlockTier {
 
     uint256 tierRange;
     uint256 additionalIssuanceCapacity;
+    mapping(address => uint256) ticketsBought;
     mapping(address => tier) userTiers;
+
+    constructor(uint256 tierRangeIn, uint256 additionalIssuanceCapacityIn) public {
+        tierRange = tierRangeIn;
+        additionalIssuanceCapacity = additionalIssuanceCapacityIn;
+    }
 
     modifier adminOnly() {
         require(admin == msg.sender, "Only admins can run this function!");
         _;
     }
 
-    constructor(uint256 tierRangeIn, uint256 additionalIssuanceCapacityIn) public {
-        tierRange = tierRangeIn;
-        additionalIssuanceCapacity = additionalIssuanceCapacityIn;
+    function addTicketsBought(address user, uint256 quantity) public {
+        ticketsBought[user] += quantity;
+
+        if (ticketsBought[user] > 3 * tierRange) {
+            userTiers[user] = tier.diamond;
+        } else if (ticketsBought[user] > 2 * tierRange) {
+            userTiers[user] = tier.gold;
+        } else if (ticketsBought[user] > tierRange) {
+            userTiers[user] = tier.silver;
+        }
+    }
+
+    function getAdditionalIssuanceLimit(address user) public view returns(uint256) {
+        if (userTiers[user] == tier.diamond) {
+            return 3 * additionalIssuanceCapacity;
+        } else if (userTiers[user] == tier.gold) {
+            return 2 * additionalIssuanceCapacity;
+        } else if (userTiers[user] == tier.silver) {
+            return additionalIssuanceCapacity;
+        } else {
+            return 0;
+        }
     }
 }
